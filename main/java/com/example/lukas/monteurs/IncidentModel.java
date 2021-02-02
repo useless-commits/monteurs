6a46c48aa906836e04b69da00bf999aa63c81b6c
Daniël <dahnjohl@gmail.com>
2021-02-02 13:57:05 +0100

    :art: art

--- main/java/com/example/lukas/monteurs/IncidentModel.java
+++ main/java/com/example/lukas/monteurs/IncidentModel.java
@@ -1,22 +1,22 @@
-package com.example.lukas.monteurs;
+package com.example.lukas.monteurs                              ;
 
-public class IncidentModel {
-    public int id;
-    public String omschrijving;
-    public String categorie;
-    public String image_path;
-    public float longitude;
-    public float latitude;
-    public String date;
+public class IncidentModel                                      {
+    public int id                                               ;
+    public String omschrijving                                  ;
+    public String categorie                                     ;            
+    public String image_path                                    ;
+    public float longitude                                      ;
+    public float latitude                                       ;
+    public String date                                          ;
 
     public IncidentModel(int id, String omschrijving, String categorie, String  image_path, float latitude,
-                         float longitude, String date) {
-        this.id =  id;
-        this.categorie =  categorie;
-        this.omschrijving = omschrijving;
-        this.image_path = image_path;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.date = date;
-    }
-}
+                         float longitude, String date)          {
+        this.id             =  id                               ;
+        this.categorie      =  categorie                        ;
+        this.omschrijving   = omschrijving                      ;
+        this.image_path     = image_path                        ;
+        this.latitude       = latitude                          ;      
+        this.longitude      = longitude                         ;
+        this.date           = date                              ;
+                                                                }
+                                                                }
